2d5f84590a92139dbd6d8866b3bcd2c731c62b6d
Jang hee ju <s6000000@naver.com>
2024-12-18 11:39:53 +0900

    Step07에 MainClass05 완성

--- Step07_Array/src/test/main/MainClass05.java
+++ Step07_Array/src/test/main/MainClass05.java
@@ -13,15 +13,28 @@ public class MainClass05 {
 		 */
 		
 		// Bike[] bikes = {new Bike(), new Bike(), new Bike()}; //bike타입, 배열을 선언할때 방마다 Bike라는 객체가 생성.
-		Bike[] bikes = new Bike[3]; //bike타입,얘는 null로 초기화 된...배열을 선언할때,,방3개짜리 빈배열객체
+		Bike[] bikes = new Bike[3]; //bike타입,얘는 null로 초기화 된 참조값이 담기지않은 방3개짜리 빈배열객체가 생김
 		
 		for (int i=0; i<bikes.length; i++ ) {
 			bikes[i]=new Bike();
 		}
-		for (int i=0; i<bikes.length; i++ ) {
-			bikes[i].ride();  
-		}
 		
+//		bikes[0]=new Bike();
+//		bikes[1]=new Bike();
+//		bikes[2]=new Bike();
+
+		for (int i=0; i<bikes.length; i++ ) {
+			//bikes[i].ride();   //한줄로하기 
+			//i번째 방에 있는 Bike 객체의 참조값얻어오기
+			Bike tmp = bikes[i];
+			//tmp에 있는 Bike 객체의 .ride() 메소드 호출하기
+			tmp.ride();		
+		}
+		System.out.println();
+		//확장for문 ..for문에우측에는 배열을 참조, 좌측에는 타입변수명
+		for(Bike tmp:bikes) {
+			tmp.ride();
+		}
 		
 	}
 }
